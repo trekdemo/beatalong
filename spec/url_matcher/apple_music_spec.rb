8f0dfa123a190f9e9532dbd433d3ce308da4beb0
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2015-10-10 08:34:01 +0200

    Add more examples to UrlMatcher::AppleMusic

--- spec/url_matcher/apple_music_spec.rb
+++ spec/url_matcher/apple_music_spec.rb
@@ -4,10 +4,18 @@ module UrlMatcher
   RSpec.describe AppleMusic do
     describe '#match?' do
       [
+        # artist
+        'https://itunes.apple.com/nl/artist/kellerkind/id290891328?l=en',
+        # album
+        'https://itunes.apple.com/nl/album/caracal-deluxe/id1002029534?l=en',
+        'http://itunes.apple.com/nl/album/caracal-deluxe/id1002029534?l=en',
+        # track
         'https://itun.es/nl/EhCU7?i=1002030097',
-        'https://itunes.apple.com/nl/album/caracal-deluxe/id1002029534?i=1002030097&l=en',
         'http://itun.es/nl/EhCU7?i=1002030097',
+        'https://itunes.apple.com/nl/album/caracal-deluxe/id1002029534?i=1002030097&l=en',
         'http://itunes.apple.com/nl/album/caracal-deluxe/id1002029534?i=1002030097&l=en',
+        # playlist
+        'https://itunes.apple.com/nl/playlist/amsterdam-dance-event-amsterdam/idpl.633ff1b81d3046138a4b384c717762d9?l=en',
       ].each do |url|
         context "when #{url.inspect}" do
           subject { described_class.new(url) }
