fe44a5520b79daff4bd70a6852f805a7d7199be9
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2015-10-11 13:00:21 +0200

    Add validation to ProviderIdentity

--- lib/provider_identity.rb
+++ lib/provider_identity.rb
@@ -1,9 +1,12 @@
 class ProviderIdentity
+  VALID_KIND = %w[artist album track playlist]
+
   attr_reader :provider, :id, :kind, :country_code
+
   def initialize(provider:, id:, kind:, country_code:)
     @provider = provider
     @id = id
-    @kind = kind
+    @kind = validated_kind(kind.to_s)
     @country_code = country_code
   end
 
@@ -13,5 +16,11 @@ class ProviderIdentity
 
   def ==(other)
     to_a == other.to_a
+  end
+
+  def validated_kind(kind)
+    return kind if VALID_KIND.include?(kind)
+
+    fail ArgumentError, "#{kind.inspect} is not a valid kind!"
   end
 end
