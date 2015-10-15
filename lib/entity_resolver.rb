42fa7d4c967ba2f9d75fb2fff73c6fb4524535e0
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2015-10-15 22:35:14 +0200

    Use new structure to resolve identities from urls

--- lib/entity_resolver.rb
+++ lib/entity_resolver.rb
@@ -1,44 +1,24 @@
-require 'url_matcher/apple_music'
-require 'url_matcher/deezer'
-# require 'url_matcher/rdio'
-require 'url_matcher/spotify'
 require 'url_identity_resolver/null'
 require 'url_identity_resolver/apple_music'
 require 'url_identity_resolver/deezer'
 require 'url_identity_resolver/spotify'
 # require 'url_identity_resolver/rdio'
+# require 'url_identity_resolver/google_play_music'
 
 class EntityResolver
   RESOLVERS = [
-    UrlMatcher::AppleMusic,
-    UrlMatcher::Deezer,
-    # UrlMatcher::Rdio,
-    UrlMatcher::Spotify,
+    UrlIdentityResolver::AppleMusic,
+    UrlIdentityResolver::Deezer,
+    # UrlIdentityResolver::GooglePlayMusic,
+    # UrlIdentityResolver::Rdio,
+    UrlIdentityResolver::Spotify,
+    UrlIdentityResolver::Null, # This must be the last
   ]
 
-  attr_reader :url
-
-  def initialize(url)
-    @url = url
-  end
-
-  def identity
-    identity_resolver.identity
-  end
-
-  private
-
-  def identity_resolver
-    resolver_name = provider_name || 'Null'
-    resolver_class = Object.const_get("UrlIdentityResolver::#{resolver_name}")
-    resolver_class.new(url)
-  end
-
-  def provider_name
+  def self.identity_from(url)
     RESOLVERS
-      .find { |matcher| matcher.match?(url) }
-      .to_s
-      .split('::')
-      .last
+      .find { |resolver| resolver.match?(url) }
+      .new(url)
+      .identity
   end
 end
