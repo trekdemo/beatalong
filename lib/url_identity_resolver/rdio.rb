7fc69ad5a65eb23f6508187e7e64846a33e7b670
Miklos Beothy <beothy.miklos@gmail.com>
2015-10-16 18:27:37 +0200

    url_identity_resolver for rdio

--- lib/url_identity_resolver/rdio.rb
+++ lib/url_identity_resolver/rdio.rb
@@ -1,5 +1,9 @@
+require 'url_identity_resolver/base'
+
 module UrlIdentityResolver
   class Rdio
+    include Base
+
     def self.match?(url)
       !!(
         url =~ /https?:\/\/(www\.)?rdio\.com/ ||
@@ -7,8 +11,15 @@ module UrlIdentityResolver
       )
     end
 
-    def initialize(*); end
-    def call; end
-    def identity; end
+    def call
+      short_match = url.to_s.match(/https?:\/\/rd.io\/x\/(?<id>\w+)/)
+
+      self.id = if short_match
+        short_match[:id]
+      else
+        url.path
+      end
+      true
+    end
   end
 end
