97243188c142b9db30c09655476c6d8105c5d3cd
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2015-10-21 21:59:22 +0200

    Refactor ErrorHandlerMiddleware

--- lib/middleware/error_handler_middleware.rb
+++ lib/middleware/error_handler_middleware.rb
@@ -4,24 +4,17 @@ class ErrorHandlerMiddleware
   end
 
   def call(env)
-    begin
-      status, headers, response = @app.call(env)
-    rescue Beatalong::UrlNotSupported
-      message = "The specified url isn't supported!"
-    rescue Beatalong::KindNotSupported => e
-      message = "'#{e.unsupported_kind.titleize}' entities are not supported currently! Please share artists, albums or tracks!"
-    rescue Beatalong::IdentityNotFound
-      message = "No supported entity found on the specified url"
-    end
+    @app.call(env)
+  rescue Beatalong::UrlNotSupported
+    respond_with_error "The specified url isn't supported!"
+  rescue Beatalong::KindNotSupported => e
+    respond_with_error "'#{e.unsupported_kind.titleize}' entities are not supported currently! Please share artists, albums or tracks!"
+  rescue Beatalong::IdentityNotFound
+    respond_with_error "Not supported entity found on the specified url"
+  end
 
-    if message
-      env['x-rack.flash'].error = message
-      IndexApp.new.call(env)
-
-      #[301, {'Location' => "/i"}, []]
-    else
-      [status, headers, response]
-    end
-
+  def respond_with_error(message)
+    env['x-rack.flash'].error = message
+    IndexApp.new.call(env)
   end
 end
