78647e9bfe7a436015465b788c8029752de3502b
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2015-10-21 22:12:55 +0200

    Fix specs

--- lib/middleware/error_handler_middleware.rb
+++ lib/middleware/error_handler_middleware.rb
@@ -6,14 +6,14 @@ class ErrorHandlerMiddleware
   def call(env)
     @app.call(env)
   rescue Beatalong::UrlNotSupported
-    respond_with_error "The specified url isn't supported!"
+    respond_with_error env, "The specified url isn't supported!"
   rescue Beatalong::KindNotSupported => e
-    respond_with_error "'#{e.unsupported_kind.titleize}' entities are not supported currently! Please share artists, albums or tracks!"
+    respond_with_error env, "'#{e.unsupported_kind.titleize}' entities are not supported currently! Please share artists, albums or tracks!"
   rescue Beatalong::IdentityNotFound
-    respond_with_error "Not supported entity found on the specified url"
+    respond_with_error env, "Not supported entity found on the specified url"
   end
 
-  def respond_with_error(message)
+  def respond_with_error(env, message)
     env['x-rack.flash'].error = message
     IndexApp.new.call(env)
   end
