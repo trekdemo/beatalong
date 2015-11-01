77fe1e4aba44b78d472f3abfd702473998ac7402
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2015-11-01 02:14:52 +0100

    Implement playlist lookup

--- lib/api/apple_music.rb
+++ lib/api/apple_music.rb
@@ -1,3 +1,4 @@
+require 'csv'
 require 'active_support/core_ext/hash/slice'
 require 'api/base'
 
@@ -5,6 +6,12 @@ module Api
   # Offical docs:
   #   https://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html#searching
   class AppleMusic
+    STORE_FRONTS = Hash[
+      CSV
+        .read(File.expand_path('../../../config/itunes_store_fronts.csv', __FILE__))
+        .map { |(country_code, id, _country_name)| [country_code.downcase.strip, id.strip] }
+    ]
+
     include ::Api::Base
     base_uri 'https://itunes.apple.com'
 
@@ -12,11 +19,15 @@ module Api
       limit lang version explicit]
 
     def find(identity)
-      get('/lookup', {
-        id: identity.id,
-        entity: itunes_kind(identity.kind),
-        country: identity.country_code || country_code,
-      })
+      case identity.kind
+      when 'playlist' then lookup_playlist(identity)
+      else
+        get('/lookup', {
+          id: identity.id,
+          entity: itunes_kind(identity.kind),
+          country: identity.country_code || country_code,
+        })
+      end
     end
 
     def search(entity)
@@ -30,19 +41,38 @@ module Api
 
     private
 
+    # curl \
+    # -H "X-Apple-Store-Front: 143452-2,32 t:music2" \
+    # -H "User-Agent: iTunes/12.3.1 (Macintosh; # OS X 10.11.1) AppleWebKit/601.2.7.2" \
+    # "https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPlaylist?id=pl.5e01462edfd74e23b80e38b9982b30d5" > playlist.json
+    def lookup_playlist(identity)
+      store_front = "#{STORE_FRONTS[identity.country_code]},32 t:music2"
+      get(
+        'https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPlaylist',
+        {id: identity.id},
+        {'X-Apple-Store-Front' => store_front}
+      )
+    end
+
     def search_term(entity)
       clean_api_query_string([entity.artist, entity.album, entity.track].compact.join(' - '))
     end
 
-    def get(path, query)
-      response = self.class.get(path, query: query)
+    def get(path, query, headers = {})
+      response = self.class.get(path, query: query, headers: headers)
       if (error_message = response['errorMessage'])
         fail StandardError, "API error: #{error_message}"
       end
 
-      response["results"]
-        .map(&method(:format_result))
-        .first
+      if response.has_key?('results')
+        response['results']
+          .map(&method(:build_entity))
+          .first
+      elsif response.has_key?('storePlatformData')
+        build_playlist(response['storePlatformData']['playlist-product']['results'][query[:id]])
+      else
+        response
+      end
     end
 
     def normalize_search_query(query)
@@ -54,19 +84,30 @@ module Api
       query.slice(*AVAILABLE_SEARCH_PARAMS)
     end
 
-    def format_result(result)
+    def build_entity(result)
       ProviderEntity.new({
-        kind: internal_kind(result['wrapperType']),
+        kind: internal_kind(result['wrapperType'] || result['kind']),
         artist: result['artistName'],
         album: result['collectionName'],
-        track: result['trackName'],
+        track: result['trackName'] || result['name'],
         url: (
           result['trackViewUrl'] ||
           result['collectionViewUrl'] ||
           result['artistViewUrl']||
-          result['artistLinkUrl']
+          result['artistLinkUrl'] ||
+          result['url']
         ),
         cover_image_url: result['artworkUrl100'].to_s.sub('100x100', '420x420'),
+      })
+    end
+
+    def build_playlist(response)
+      ProviderPlaylist.new({
+        title: response['nameRaw'],
+        author: response['curatorName'],
+        cover_image_url: response['artwork']['url'],
+        url: response['url'],
+        tracks: response['children'].values.map(&method(:build_entity))
       })
     end
 
@@ -81,6 +122,7 @@ module Api
     def internal_kind(itunes_kind)
       case itunes_kind
       when 'collection' then 'album'
+      when 'song' then 'track'
       else itunes_kind
       end
     end
