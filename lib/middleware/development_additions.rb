0f3a9be27f685730925063f1d24d579234400776
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2015-11-01 03:30:03 +0100

    Add path to flush redis in development

--- lib/middleware/development_additions.rb
+++ lib/middleware/development_additions.rb
@@ -16,6 +16,7 @@ class DevelopmentAdditions
   def build_extended_app(app)
     Rack::Builder.new do
       use Rack::ShowExceptions
+      map('/flushall') { $redis.flushall }
       run app
     end
   end
